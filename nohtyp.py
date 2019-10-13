fd10b6794138afd8fc82113b9289641dc2439599
L3viathan <git@l3vi.de>
2019-10-13 18:03:42 +0200

    Cache modules, etc.

--- nohtyp.py
+++ nohtyp.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 opmap = {code: name for name, code in dis.opmap.items()}
+mod_cache = {}
 
 
 def pairwise(iterable):
@@ -18,25 +19,39 @@ def pairwise(iterable):
         yield first, next(iterator)
 
 
-class Module:
-    def __init__(self, name, mappings=None):
+class Namespace:
+    def __init__(self, name, mappings=None, parent=None):
+        self.__parent = parent
         self.__name__ = name
-        for k, v in builtins.__dict__.items():
-            setattr(self, k, v)
         if mappings:
             self.__dict__.update(**mappings)
 
+    def __getattr__(self, attribute):
+        if self.__parent:
+            log.warn(f"Can't find {attribute} on {self.__name__}, looking at parent ({self.__parent.__name__})")
+            return getattr(self.__parent, attribute)
+        raise AttributeError(f"Can't find {attribute} on {self.__name__}")
+
 
 class Python:
-    def __init__(self, code, my_name="unknown", mappings=None):
+    def __init__(self, code, my_name="unknown", mappings=None, module=None):
         for k in dir(code):
             if k.startswith("co_"):
                 _, __, name = k.partition("_")
                 value = getattr(code, k)
                 setattr(self, name, value)
+        self._type = type
         self._stack = []
         self.varnames = list(self.varnames)
-        self._mappings = Module(my_name, mappings=mappings)
+        if module:  # meaning self is _not_ a module
+            # warning: module can be either real, builtin module, or own module
+            self._mappings = Namespace(
+                my_name, parent=module._mappings, mappings=mappings
+            )
+        else:
+            self._mappings = Namespace(my_name, parent=builtins)
+            mod_cache[my_name] = self
+        self._module = module or self
         self._return = None
         self.ip = 0
 
@@ -46,7 +61,7 @@ class Python:
             setattr(self._mappings, name, arg)
         self._return = self._mappings
         while True:
-            code = self.code[self.ip:self.ip+2]
+            code = self.code[self.ip : self.ip + 2]
             if not code:
                 break
             opcode, arg = code
@@ -86,10 +101,24 @@ class Python:
     def CALL_FUNCTION(self, arg):
         args = reversed([self._stack.pop() for _ in range(arg)])
         function = self._stack.pop()
+        log.info("Calling function %s", function.__name__)
         if isinstance(function, (type(print), type)):
             self._stack.append(function(*args))
         else:
-            self._stack.append(Python(function.__code__, function.__name__, mappings=function.__globals__)(*args))
+            mapping = {}
+            for key, val in zip(function.__code__.co_cellvars, function.__defaults__):
+                mapping[key] = val
+            # mapping.update(function.__globals__)
+            import ipdb; ipdb.set_trace()
+            self._stack.append(
+                Python(
+                    function.__code__,
+                    function.__name__,
+                    mappings=mapping,
+                    # module=self._module,
+                    module=mod_cache[function.__module__]
+                )(*args)
+            )
 
     def LOAD_FAST(self, arg):
         self._stack.append(getattr(self._mappings, self.varnames[arg]))
@@ -106,17 +135,22 @@ class Python:
 
     def IMPORT_NAME(self, arg):
         name = self.names[arg]
+        if name in mod_cache:
+            return self._stack.append(mod_cache[name])
         try:
-            self._stack.append(run(name))
+            # TODO: module cache (for globals in modules)
+            module = run(name)
         except FileNotFoundError:
-            # FIXME: Either not compiled yet, or builtin. For now, just import the actual Python names
-            # setattr(self._mappings, name, __import__(name))
-            self._stack.append(__import__(name))
+            module = __import__(name)
+        mod_cache[name] = module
+        self._stack.append(module)
 
     def IMPORT_FROM(self, arg):
         name = self.names[arg]
         module = self._stack.pop()
-        setattr(self._mappings, name, getattr(module, name))
+        self._stack.append(module)
+        self._stack.append(getattr(module, name))
+        # setattr(self._mappings, name, getattr(module, name))
 
     def LOAD_ATTR(self, arg):
         attr = self.names[arg]
@@ -206,7 +240,9 @@ class Python:
         elif op == "is not":
             self._stack.append(x is not y)
         elif op == "exception match":
-            import ipdb; ipdb.set_trace()
+            import ipdb
+
+            ipdb.set_trace()
             ...
         else:
             raise RuntimeError("This probably shouldn't happen")
@@ -239,11 +275,23 @@ class Python:
     def BUILD_STRING(self, arg):
         self._stack.append("".join(self._stack.pop() for _ in range(arg)))
 
+    def LOAD_DEREF(self, arg):
+        self._stack.append(getattr(self._mappings, self.cellvars[arg]))
+
+    def JUMP_IF_TRUE_OR_POP(self, arg):
+        val = self._stack.pop()
+        if val:
+            self.ip.seek(arg)
+            self._stack.append(val)
+
+    def STORE_DEREF(self, arg):
+        setattr(self._mappings, self.cellvars[arg], self._stack.pop())
+
 
 Function = type(pairwise)
 
 
-def run(filename, try_compile=True):
+def run(filename, try_compile=True, is_main=False):
     log.debug("trying to run %s", filename)
     cached = glob.glob(f"__pycache__/{filename}.cpython-*.pyc")
     if not cached:
@@ -257,9 +305,9 @@ def run(filename, try_compile=True):
     with open(cached, "rb") as f:
         f.seek(16)  # ignore magic + timestamp
         code = marshal.load(f)
-        interpreter = Python(code, filename)
+        interpreter = Python(code, "__main__" if is_main else filename)
         return interpreter()
 
 
-if __name__ == '__main__':
-    run("nohtyp")
+if __name__ == "__main__":
+    run("curry", is_main=True)
